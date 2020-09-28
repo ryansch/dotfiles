617793e31aeee5f50e36580ba746aa8db2a500a7
Ryan Schlesinger <ryan@ryanschlesinger.com>
2020-09-28 10:18:27 -0700

    Ruby path is now managed with a tool from work

--- zsh.after/envvars.zsh
+++ zsh.after/envvars.zsh
@@ -1,4 +1,4 @@
-export PATH=$HOME/bin:/usr/local/opt/ruby/bin:/usr/local/lib/ruby/gems/2.6.0/bin:$PATH:/usr/local/bin
+export PATH=$HOME/bin:$PATH:/usr/local/bin
 export EDITOR="/usr/local/bin/nvim"
 export NODE_PATH=/usr/local/lib/node_modules
 export XML_CATALOG_FILES="/usr/local/etc/xml/catalog"
