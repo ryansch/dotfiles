aa5f10addba7226f3ae53b811bfeea141b45f7d4
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-10-07 23:30:16 +0000

    Lots of alias changes

--- zsh.after/alias.zsh
+++ zsh.after/alias.zsh
@@ -1,6 +1,18 @@
-eval "$(hub alias -s)"
 alias vim="nvim"
-alias mux="tmuxinator"
 alias ls="exa -g --git -b"
 alias cd="z"
 alias cdi="zi"
+
+# mimic vim functions
+alias :q='exit'
+
+# moar bat
+alias cat="bat"
+alias rg="batgrep"
+alias man="batman"
+alias watch="batwatch"
+
+# Global aliases
+alias -g ...='../..'
+alias -g ....='../../..'
+alias -g .....='../../../..'
