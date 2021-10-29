6fe4e3938f00c3613fb04b584819e4f8a626eb87
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-10-29 22:24:59 +0000

    Reconfigure telescope

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -50,8 +50,71 @@ require('lualine').setup {
   extensions = {'fugitive'}
 }
 
+local actions = require('telescope.actions')
+require('telescope').setup {
+  defaults = {
+    preview = {
+      timeout = 500,
+      msg_bg_fillchar = "",
+    },
+    layout_strategy = "horizontal",
+    layout_config = {
+      horizontal = {
+        width_padding = 0.04,
+        height_padding = 0.1,
+        preview_width = 0.6,
+      },
+    },
+    file_ignore_patterns = {
+      "^.git/",
+    },
+    vimgrep_arguments = {
+      "rg",
+      "--color=never",
+      "--no-heading",
+      "--with-filename",
+      "--line-number",
+      "--column",
+      "--smart-case",
+      "--hidden",
+      "--no-ignore",
+      "--follow",
+      "--glob '!.git/*'",
+    },
+    color_devicons = true,
+    mappings = {
+      i = {
+        ["<esc>"] = actions.close,
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+    dynamic_preview_title = true,
+  },
+  extensions = {
+    fzf = {
+      fuzzy = true,                    -- false will only do exact matching
+      override_generic_sorter = true,  -- override the generic sorter
+      override_file_sorter = true,     -- override the file sorter
+      case_mode = "smart_case",        -- or "ignore_case" or "respect_case"
+      -- the default case_mode is "smart_case"
+    }
+  }
+}
+
 -- Load fzf native extension for telescope
 require('telescope').load_extension('fzf')
+
+-- Add leader shortcuts
+vim.api.nvim_set_keymap('n', '<leader>b', [[<cmd>lua require('telescope.builtin').buffers()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>t', [[<cmd>lua require('telescope.builtin').find_files({ hidden = true, no_ignore = true, follow = true })<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin').current_buffer_fuzzy_find()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
 
 -- Create window splits easier. The default
