a244a2b7c47b8fc9a0f8909047ddaec7d22060cc
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-12-20 21:37:28 +0000

    Fix startify

--- nvim/lua/settings.lua
+++ nvim/lua/settings.lua
@@ -112,7 +112,7 @@ vim.api.nvim_set_keymap('n', '<leader>sb', [[<cmd>lua require('telescope.builtin
 vim.api.nvim_set_keymap('n', '<leader>sh', [[<cmd>lua require('telescope.builtin').help_tags()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>st', [[<cmd>lua require('telescope.builtin').tags()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>sd', [[<cmd>lua require('telescope.builtin').grep_string()<CR>]], { noremap = true, silent = true })
-vim.api.nvim_set_keymap('n', '<leader>sp', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>fg', [[<cmd>lua require('telescope.builtin').live_grep()<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>so', [[<cmd>lua require('telescope.builtin').tags{ only_current_buffer = true }<CR>]], { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>?', [[<cmd>lua require('telescope.builtin').oldfiles()<CR>]], { noremap = true, silent = true })
 
@@ -278,3 +278,8 @@ vim.opt.listchars:append("eol:↴")
 require("indent_blankline").setup {
     show_end_of_line = true,
 }
+
+-- vim-startify
+vim.g.startify_change_to_vcs_root = 1
+
+-- local hilens = require('hilens').setup
