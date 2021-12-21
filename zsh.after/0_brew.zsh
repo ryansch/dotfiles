d45479fa6312d9a45fb86331dd97ae7f81570c37
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-12-21 13:33:23 -0400

    Fixes for gh, brew, hammerspoon

--- zsh.after/0_brew.zsh
+++ zsh.after/0_brew.zsh
@@ -3,3 +3,7 @@ if [[ "$OS" == "Linux" ]]; then
   eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
   export WHALEBREW_INSTALL_PATH=/home/linuxbrew/.linuxbrew/bin
 fi
+
+if [[ "$OS" == "Darwin" ]]; then
+  eval "$(/opt/homebrew/bin/brew shellenv)"
+fi
