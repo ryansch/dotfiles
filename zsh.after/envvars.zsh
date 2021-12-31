c6364e050c52219027558d84c8daf3e5c89feb66
Ryan Schlesinger <ryan@ryanschlesinger.com>
2021-12-31 16:55:40 -0400

    Add python and rubygems paths

--- zsh.after/envvars.zsh
+++ zsh.after/envvars.zsh
@@ -1,7 +1,7 @@
 export FLYCTL_INSTALL="/Users/ryansch/.fly"
 
 typeset -U path
-path=(~/bin ~/.local/bin "$FLYCTL_INSTALL/bin" /usr/local/opt/ruby/bin "$path[@]" /usr/local/sbin /usr/local/bin)
+path=(~/bin ~/.local/bin /opt/homebrew/opt/python@3.9/libexec/bin "$FLYCTL_INSTALL/bin" /opt/homebrew/opt/ruby/bin /opt/homebrew/lib/ruby/gems/3.0.0/bin "$path[@]" /usr/local/sbin /usr/local/bin)
 
 export EDITOR="$(which nvim)"
 export NODE_PATH=/usr/local/lib/node_modules
