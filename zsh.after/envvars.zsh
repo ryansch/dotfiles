b5c8ff2d67ccfcbf576ec62b9b461ea1a05d0ac6
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-01-01 17:36:16 -0400

    Use homebrew_location function

--- zsh.after/envvars.zsh
+++ zsh.after/envvars.zsh
@@ -1,7 +1,9 @@
 export FLYCTL_INSTALL="/Users/ryansch/.fly"
+source ${HOME}/.local/share/sh/homebrew.sh
+homebrew_dir=$(homebrew_location)
 
 typeset -U path
-path=(~/bin ~/.local/bin /opt/homebrew/opt/python@3.9/libexec/bin "$FLYCTL_INSTALL/bin" /opt/homebrew/opt/ruby/bin /opt/homebrew/lib/ruby/gems/3.0.0/bin "$path[@]" /usr/local/sbin /usr/local/bin)
+path=(~/bin ~/.local/bin /opt/homebrew/opt/python@3.9/libexec/bin "$FLYCTL_INSTALL/bin" "${homebrew_dir}/opt/ruby/bin" "${homebrew_dir}/lib/ruby/gems/3.0.0/bin" "$path[@]" /usr/local/sbin /usr/local/bin)
 
 export EDITOR="$(which nvim)"
 export NODE_PATH=/usr/local/lib/node_modules
