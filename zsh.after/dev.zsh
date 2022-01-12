00cefb9e17bc659028fbdc77e9d5dbbd7d9cfb12
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-01-12 11:15:22 -0400

    Fix dev/dash install path

--- zsh.after/dev.zsh
+++ zsh.after/dev.zsh
@@ -1,4 +1,4 @@
 # Set up dev/dash
-if [ -f "/usr/local/dev-env/ansible/dash_profile" ]; then
-	source /usr/local/dev-env/ansible/dash_profile
+if [ -f "/opt/dev-env/ansible/dash_profile" ]; then
+	source /opt/dev-env/ansible/dash_profile
 fi
