52e1a4dbe0c0bb77795995196e199c73a23d79a9
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-01-27 19:50:17 +0000

    Use .local/bin

--- zsh.after/envvars.zsh
+++ zsh.after/envvars.zsh
@@ -3,7 +3,7 @@ source ${HOME}/.local/share/sh/homebrew.sh
 homebrew_dir=$(homebrew_location)
 
 typeset -U path
-path=(~/bin ~/.local/bin /opt/homebrew/opt/python@3.9/libexec/bin "$FLYCTL_INSTALL/bin" "${homebrew_dir}/opt/ruby/bin" "${homebrew_dir}/lib/ruby/gems/3.0.0/bin" "$path[@]" /usr/local/sbin /usr/local/bin)
+path=(~/.local/bin /opt/homebrew/opt/python@3.9/libexec/bin "$FLYCTL_INSTALL/bin" "${homebrew_dir}/opt/ruby/bin" "${homebrew_dir}/lib/ruby/gems/3.0.0/bin" "$path[@]" /usr/local/sbin /usr/local/bin)
 
 export EDITOR="$(which nvim)"
 export NODE_PATH=/usr/local/lib/node_modules
