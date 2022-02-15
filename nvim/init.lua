59ed5225dd6e5970a4119747ef260c69864a5342
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-02-15 13:28:17 +0000

    Move key bindings up

--- nvim/init.lua
+++ nvim/init.lua
@@ -76,6 +76,77 @@ end)
 
 require('global')
 
+-- which-key
+local wk = require('which-key')
+local t_builtin = require('telescope.builtin')
+local function find_files()
+  t_builtin.find_files({
+    hidden = true,
+    no_ignore = true,
+  })
+end
+wk.setup {
+  window = {
+    border = "single"
+  }
+}
+wk.register({
+  f = {
+    name = "telescope finders",
+    b = { t_builtin.buffers, "Lists open buffers" },
+    f = { find_files, "Find file" },
+    h = { t_builtin.help_tags, "Lists available help tags " },
+    t = { t_builtin.tags, "Lists tags in current directory " },
+    g = { t_builtin.live_grep, "Search for a string" },
+    ['?'] = { t_builtin.oldfiles, "Lists previously open files" },
+  },
+  l = {
+    name = "telescope LSP",
+    a = { t_builtin.diagostics, "Lists diagnostics" },
+    d = { t_builtin.lsp_definitions, "Goto definition" },
+    D = { t_builtin.lsp_type_definitions, "Goto type definition" },
+    r = { t_builtin.lsp_references, "Lists LSP references" },
+    i = { t_builtin.lsp_implementations, "Goto implementation" },
+    s = { t_builtin.lsp_document_symbols, "Lists LSP document symbols" },
+    w = { t_builtin.lsp_dynamic_workspace_symbols, "Dynamically Lists LSP for all workspace symbols" },
+  },
+  r = {
+    name = "test runners",
+  },
+  d = {
+    name = "debugger",
+    l = {
+      name = "lua",
+      l = { "<Plug>(Luadev-RunLine)", "Execute the current line" },
+      r = { "<Plug>(Luadev-Run)", "Execute lua code over a movement or text object" },
+      w = { "<Plug>(Luadev-RunWord)", "Eval identifier under cursor" },
+    },
+  },
+}, { prefix = "<leader>" })
+
+-- Gitsigns
+require('gitsigns').setup {
+  signs = {
+    add = { text = '+' },
+    change = { text = '~' },
+    delete = { text = '_' },
+    topdelete = { text = '‾' },
+    changedelete = { text = '~' },
+  },
+}
+
+-- Telescope
+require('telescope').setup {
+  defaults = {
+    mappings = {
+      i = {
+        ['<C-u>'] = false,
+        ['<C-d>'] = false,
+      },
+    },
+  },
+}
+
 --Set highlight on search
 vim.o.hlsearch = false
 
@@ -210,77 +281,6 @@ end
 
 -- gutentags
 vim.g.gutentags_cache_dir = "~/.cache/gutentags"
-
--- which-key
-local wk = require('which-key')
-local t_builtin = require('telescope.builtin')
-local function find_files()
-  t_builtin.find_files({
-    hidden = true,
-    no_ignore = true,
-  })
-end
-wk.setup {
-  window = {
-    border = "single"
-  }
-}
-wk.register({
-  f = {
-    name = "telescope finders",
-    b = { t_builtin.buffers, "Lists open buffers" },
-    f = { find_files, "Find file" },
-    h = { t_builtin.help_tags, "Lists available help tags " },
-    t = { t_builtin.tags, "Lists tags in current directory " },
-    g = { t_builtin.live_grep, "Search for a string" },
-    ['?'] = { t_builtin.oldfiles, "Lists previously open files" },
-  },
-  l = {
-    name = "telescope LSP",
-    a = { t_builtin.diagostics, "Lists diagnostics" },
-    d = { t_builtin.lsp_definitions, "Goto definition" },
-    D = { t_builtin.lsp_type_definitions, "Goto type definition" },
-    r = { t_builtin.lsp_references, "Lists LSP references" },
-    i = { t_builtin.lsp_implementations, "Goto implementation" },
-    s = { t_builtin.lsp_document_symbols, "Lists LSP document symbols" },
-    w = { t_builtin.lsp_dynamic_workspace_symbols, "Dynamically Lists LSP for all workspace symbols" },
-  },
-  r = {
-    name = "test runners",
-  },
-  d = {
-    name = "debugger",
-    l = {
-      name = "lua",
-      l = { "<Plug>(Luadev-RunLine)", "Execute the current line" },
-      r = { "<Plug>(Luadev-Run)", "Execute lua code over a movement or text object" },
-      w = { "<Plug>(Luadev-RunWord)", "Eval identifier under cursor" },
-    },
-  },
-}, { prefix = "<leader>" })
-
--- Gitsigns
-require('gitsigns').setup {
-  signs = {
-    add = { text = '+' },
-    change = { text = '~' },
-    delete = { text = '_' },
-    topdelete = { text = '‾' },
-    changedelete = { text = '~' },
-  },
-}
-
--- Telescope
-require('telescope').setup {
-  defaults = {
-    mappings = {
-      i = {
-        ['<C-u>'] = false,
-        ['<C-d>'] = false,
-      },
-    },
-  },
-}
 
 -- Enable telescope fzf native
 require('telescope').load_extension 'fzf'
