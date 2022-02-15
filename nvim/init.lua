98cf88246531fd70fe1c766b08525789918a9e27
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-02-15 13:27:08 +0000

    Extract treesitter module

--- nvim/init.lua
+++ nvim/init.lua
@@ -285,96 +285,13 @@ require('telescope').setup {
 -- Enable telescope fzf native
 require('telescope').load_extension 'fzf'
 
--- Treesitter configuration
--- Parsers must be installed manually via :TSInstall
-require('nvim-treesitter.configs').setup {
-  ensure_installed = {
-    'bash',
-    'comment',
-    'css',
-    'dockerfile',
-    -- 'elixir',
-    'erlang',
-    'go',
-    'graphql',
-    'heex',
-    'html',
-    'http',
-    'javascript',
-    'json',
-    'json5',
-    'lua',
-    'make',
-    'markdown',
-    'nix',
-    'python',
-    'regex',
-    'ruby',
-    'rust',
-    'toml',
-    'typescript',
-    'vim',
-    'yaml',
-  },
-  highlight = {
-    enable = true, -- false will disable the whole extension
-    disable = {
-      "elixir"
-    }
-  },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = 'gnn',
-      node_incremental = 'grn',
-      scope_incremental = 'grc',
-      node_decremental = 'grm',
-    },
-  },
-  indent = {
-    enable = false,
-  },
-  textobjects = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in textobjects.scm
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-      },
-    },
-    move = {
-      enable = true,
-      set_jumps = true, -- whether to set jumps in the jumplist
-      goto_next_start = {
-        [']m'] = '@function.outer',
-        [']]'] = '@class.outer',
-      },
-      goto_next_end = {
-        [']M'] = '@function.outer',
-        [']['] = '@class.outer',
-      },
-      goto_previous_start = {
-        ['[m'] = '@function.outer',
-        ['[['] = '@class.outer',
-      },
-      goto_previous_end = {
-        ['[M'] = '@function.outer',
-        ['[]'] = '@class.outer',
-      },
-    },
-  },
-}
-
 -- Diagnostic keymaps
 vim.api.nvim_set_keymap('n', '<leader>e', '<cmd>lua vim.diagnostic.open_float()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { noremap = true, silent = true })
 vim.api.nvim_set_keymap('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { noremap = true, silent = true })
 
+require("treesitter").setup()
 require("lsp").setup()
 
 -- vim: ts=2 sts=2 sw=2 et
