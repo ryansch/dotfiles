1ac1dad6f5d93883b48ff0dff2695f4e3395d1eb
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-03-29 15:31:49 +0000

    Fix load order on bootstrap

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -3,9 +3,8 @@ if not ok then
 	print("Unable to require titan.global")
 end
 
-local user_packer_config = require("user.packer-config").config()
-
 local packer_bootstrap = require("user.packer-bootstrap")
+local user_packer_config = require("user.packer-config").config()
 packer_bootstrap.startup(user_packer_config)
 
 local titan = require("titan")
