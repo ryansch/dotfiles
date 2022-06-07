06fe7ab8037f8c021798e12ae72d886d606e424b
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-06-07 13:15:14 +0000

    Yell loudly if asdf is installed via linuxbrew

--- config/zsh/zsh.after/asdf.zsh
+++ config/zsh/zsh.after/asdf.zsh
@@ -1,7 +1,8 @@
 OS="$(uname)"
 if [[ "$OS" == "Linux" ]]; then
 	if [ -f /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh ]; then
-		. /home/linuxbrew/.linuxbrew/opt/asdf/libexec/asdf.sh
+		echo "DO NOT USE ASDF INSTALLED IN LINUXBREW"
+		echo "Not loading asdf!"
 	elif [ -f "$HOME/.asdf/asdf.sh" ]; then
 		. "$HOME/.asdf/asdf.sh"
   fi
