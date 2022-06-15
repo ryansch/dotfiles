2860c63a6856c19276235c74bb3c8fa7686caa39
Ryan Schlesinger <ryan@ryanschlesinger.com>
2022-06-15 13:39:19 -0300

    Steal kitty icon update script from @megalithic!

---
+++ script/update_kitty_icon.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+set -euo pipefail
+
+if [[ $(uname) != "Darwin" ]]; then
+  log_warn "this script requires MacOS (darwin); skipping."
+  exit 0
+fi
+
+# HOWTO:
+# - https://www.sethvargo.com/replace-icons-osx
+# - https://github.com/DinkDonk/kitty-icon#installation
+cp .dotfiles/config/kitty/kitty.icns /Applications/kitty.app/Contents/Resources/kitty.icns
+rm /var/folders/*/*/*/com.apple.dock.iconcache
+touch /Applications/kitty.app
+killall Dock && killall Finder
