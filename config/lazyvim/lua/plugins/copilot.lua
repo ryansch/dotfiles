177e744998ec79c4525af43619f49d3b1d4f1e08
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-05-15 09:08:23 -0300

    Fix copilot bug

--- config/lazyvim/lua/plugins/copilot.lua
+++ config/lazyvim/lua/plugins/copilot.lua
@@ -1,6 +1,6 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
+    "copilot.lua",
 
     opts = function(_, opts)
       opts.filetypes = vim.tbl_extend("force", opts.filetypes or {}, {
@@ -8,6 +8,22 @@ return {
           TelescopePrompt = false,
         },
       })
+    end,
+  },
+
+  {
+    "copilot-cmp",
+
+    config = function(_, opts)
+      local copilot_cmp = require("copilot_cmp")
+      copilot_cmp.setup(opts)
+      -- attach cmp source whenever copilot attaches
+      -- fixes lazy-loading issues with the copilot cmp source
+      require("lazyvim.util").on_attach(function(client)
+        if client.name == "copilot" then
+          copilot_cmp._on_insert_enter({})
+        end
+      end)
     end,
   },
 }
