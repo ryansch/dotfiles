cda1f0f57f07589c0f73b2ab5db9c83da8798d39
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-05-31 11:14:53 -0300

    Simplify!

--- config/lazyvim/lua/plugins/snippets.lua
+++ config/lazyvim/lua/plugins/snippets.lua
@@ -1,19 +1,8 @@
 return {
   {
-    "L3MON4D3/LuaSnip",
-
-    config = function(_, opts)
-      require("luasnip").setup(opts)
-      require("luasnip").filetype_extend("ruby", { "rails" })
-    end,
-  },
-  {
     "ryansch/scissors",
 
     dev = true,
-
-    config = function()
-      require("luasnip.loaders.from_vscode").lazy_load()
-    end,
+    config = true,
   },
 }
