d2b0f6063da8ffdd4c909d24058b8ec6ecce63cb
Ryan Schlesinger <ryan@ryanschlesinger.com>
2023-11-11 17:47:21 -0400

    Update docker volume scripts

--- bin/docker-backup-volume.sh
+++ bin/docker-backup-volume.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+set -euo pipefail
+
 VOLUME=${1:?'Volume name required as first argument'}
 
 volume_switches="-v ${VOLUME}:/source/${VOLUME}"
 
 set -x
-docker run -it --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /bin/docker:/bin/docker \
-  -v $(pwd):/backup \
-  ${volume_switches} \
-  outstand/dockup:latest backup
+docker run --rm \
+	-v "$(pwd):/backup" \
+	"${volume_switches}" \
+	ryansch/dockup:latest backup
--- bin/docker-restore-volume.sh
+++ bin/docker-restore-volume.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-  echo 'Volume name required'
-  exit 1
-fi
+set -euo pipefail
 
-VOLUME="$1"
+VOLUME=${1:?'Volume name required as first argument'}
 
 volume_switches="-v ${VOLUME}:/source/${VOLUME}"
 
 set -x
-docker run -it --rm \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v /bin/docker:/bin/docker \
-  -v $(pwd):/backup \
-  ${volume_switches} \
-  outstand/dockup:latest restore
+docker run --rm \
+	-v "$(pwd):/backup" \
+	"${volume_switches}" \
+	ryansch/dockup:latest restore
