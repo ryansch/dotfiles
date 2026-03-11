c55e486951b3f86547b0c7e2e28e8750ac827ed3
Ryan Schlesinger <ryan@ryanschlesinger.com>
2026-03-11 10:40:03 -0300

    Routine env updates

--- config/lazyvim/lua/plugins/ai.lua
+++ config/lazyvim/lua/plugins/ai.lua
@@ -50,7 +50,7 @@ return {
       ---@type opencode.Opts
       vim.g.opencode_opts = {
         -- Your configuration, if any — see `lua/opencode/config.lua`, or "goto definition" on the type or field.
-        provider = {
+        server = {
           toggle = function(self)
             opencode:toggle()
           end,
